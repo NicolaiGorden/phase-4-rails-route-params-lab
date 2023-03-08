5861cc8145d512a2cdead33287b01000137a560b
Nicolai <Nicolaigorden@gmail.com>
2023-03-08 02:53:24 -0500

    commit tree 43ea51194e786e24e1cdac019057d90ee36111ca

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,12 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+    students = Student.where("lower(first_name) = ?", params[:name].downcase).or(Student.where("lower(last_name) = ?", params[:name].downcase)) if params[:name].present?
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
 end
